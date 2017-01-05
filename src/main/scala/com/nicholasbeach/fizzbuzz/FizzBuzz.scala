4367478b90310a517d47543f0d536773ff4be96e
Nicholas Beach <nicholascbeach@gmail.com>
2017-01-04 20:02:14 -0500

    Implemented fizzbuzz

--- src/main/scala/com/nicholasbeach/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/com/nicholasbeach/fizzbuzz/FizzBuzz.scala
@@ -2,8 +2,17 @@ package com.nicholasbeach.fizzbuzz
 
 object FizzBuzz {
 
-  def execute(number: Int): String = {
-    number.toString
+  def execute(numbers: List[Int]): List[String] = {
+    numbers.map(n => {
+      val isDivisibleBy3 = n % 3 == 0
+      val isDivisibleBy5 = n % 5 == 0
+      val isDivisibleBy3And5 = isDivisibleBy3 && isDivisibleBy5
+
+      if(isDivisibleBy3And5)  "fizzbuzz"
+      else if(isDivisibleBy3) "fizz"
+      else if(isDivisibleBy5) "buzz"
+      else                    n.toString
+    })
   }
 
 }
--- src/test/scala/com/nicholasbeach/fizzbuzz/FizzBuzzSpec.scala
+++ src/test/scala/com/nicholasbeach/fizzbuzz/FizzBuzzSpec.scala
@@ -2,15 +2,29 @@ package com.nicholasbeach.fizzbuzz
 
 import org.scalatest.FunSpec
 
-
 class FizzBuzzSpec extends FunSpec {
 
   describe("FizzBuzz") {
-
-    it("returns numbers") {
-      assert(FizzBuzz.execute(1) === "1")
+    it("when numbers are given returns numbers") {
+      assert(FizzBuzz.execute(List(1, 4)) === List("1", "4"))
     }
 
-  }
+    it("when multiples of 3 are given returns fizz") {
+      assert(FizzBuzz.execute(List(3, 9)) === List("fizz", "fizz"))
+    }
+
+    it("when multiples of 5 are given returns buzz") {
+      assert(FizzBuzz.execute(List(10, 25)) === List("buzz", "buzz"))
+    }
+
+    it("when multiple of 3 and 5 are given returns fizzbuzz") {
+      assert(FizzBuzz.execute(List(15, 30)) === List("fizzbuzz", "fizzbuzz"))
+    }
+
+    it("when values of mixed divisibility are given returns results in order matching the input") {
+      assert(FizzBuzz.execute(List(1, 3, 10, 15)) === List("1", "fizz", "buzz", "fizzbuzz"))
+    }
+
+ }
 
 }
